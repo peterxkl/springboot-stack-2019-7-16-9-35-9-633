0b1cabf64f0d4d2e082f81a1ff0b9c5fe2bd55e0
Dillon <18711345569@163.com>
2019-07-17 06:48:47 +0800

    finish Employee api getAllEmpolyee

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeesAPI.java
@@ -0,0 +1,26 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class EmployeesAPI {
+    private static List<Employee> employees = new ArrayList<>();
+    static {
+        employees.add(new Employee(4,"oocl1",20,"male",6000));
+        employees.add(new Employee(11,"oocl2",22,"female",6000));
+        employees.add(new Employee(15,"oocl3",20,"male",6000));
+        employees.add(new Employee(17,"oocl4",24,"female",6000));
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee(){
+        return employees;
+    }
+
+    
+}
